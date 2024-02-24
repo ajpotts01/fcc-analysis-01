2d8ea8e770d3ac7802a3e3b1468bae2f7da5213e
Alexander Potts <ajpotts01@gmail.com>
2024-02-24 06:01:13 +0000

    feat: calculations done

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,24 @@
 import numpy as np
 
 def calculate(list):
+    calc_map: dict[str, callable] = {
+        "mean": np.mean,
+        "variance": np.var,
+        "standard deviation": np.std,
+        "max": np.max,
+        "min": np.min,
+        "sum": np.sum,
+    }
+    
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    arr: np.array = np.array(list)
+    matrix: np.array = arr.reshape(3, 3)
 
-
-
+    calculations: dict[str, list] = {
+        x: [f(matrix, axis=0).tolist(), f(matrix, axis=1).tolist(), f(arr).tolist()]
+        for x, f in calc_map.items()
+    }
+    
     return calculations
